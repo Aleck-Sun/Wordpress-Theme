22400b92dc7de9619ad931d1195a76f3e61c06fa
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2021-05-01 00:23:19 -0400

    Added sidebar + widgets

--- archive.php
+++ archive.php
@@ -2,10 +2,21 @@
 
 <section class="page">
     <div class="container">
-        <h1><?php echo single_cat_title();?></h1>
-        <?php get_template_part('includes/section', 'archive'); ?>
-        <?php previous_posts_link(); ?>
-        <?php next_posts_link(); ?>
+        <div class="row">
+            <div class="col-2">
+                <?php 
+                if(is_active_sidebar('blog-sidebar')){
+                    dynamic_sidebar('blog-sidebar');
+                }
+                ?>
+            </div>
+            <div class="col-10">
+                <h1><?php echo single_cat_title();?></h1>
+                <?php get_template_part('includes/section', 'archive'); ?>
+                <?php previous_posts_link(); ?>
+                <?php next_posts_link(); ?>
+            </div>
+        </div>
     </div>
 </section>
 
--- functions.php
+++ functions.php
@@ -26,6 +26,7 @@ add_action('wp_enqueue_scripts', 'getJS');
 // Theme Options
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
+add_theme_support( 'widgets' );
 
 // Menus
 register_nav_menus( array('nav-bar' => 'nav bar location', 'mobiles-menu' => 'mobiles menu location') );
@@ -33,3 +34,16 @@ register_nav_menus( array('nav-bar' => 'nav bar location', 'mobiles-menu' => 'mo
 // Image Sizes
 add_image_size('blog-large', 100000, 400, true);
 add_image_size('blog-small', 100000, 200, true);
+
+// Side Bars
+function my_sidebars() {
+    register_sidebar(array(
+        'name' => 'Blog Sidebar',
+        'id' => 'blog-sidebar',
+        'before_title' => '<h4 class="widget-title">',
+        'after_title' => '</h4>'
+    ));
+
+}
+
+add_action('widgets_init', 'my_sidebars');
